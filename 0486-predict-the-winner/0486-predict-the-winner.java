acc7569c60ca821de1bc9569ee85e736bb852dfe
Adnan <adnanhussainbly740@gmail.com>
2026-08-01 11:01:55 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.96%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = nums[i];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                int left = nums[i] - dp[j];
+                int right = nums[j] - dp[j - 1];
+                dp[j] = Math.max(left, right);
+            }
+        }
+
+        return dp[n - 1] >= 0;
+    }
+}
