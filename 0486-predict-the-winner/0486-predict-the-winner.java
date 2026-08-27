93c34a8d4494c95ddc3f15d2aaa9b9c730cd542a
Adnan <adnanhussainbly740@gmail.com>
2026-08-27 23:22:53 +0530

    Optimize predictTheWinner method using dynamic programming

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -2,11 +2,9 @@ public class Solution {
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-
         for (int i = 0; i < n; i++) {
             dp[i] = nums[i];
         }
-
         for (int i = n - 2; i >= 0; i--) {
             for (int j = i + 1; j < n; j++) {
                 int left = nums[i] - dp[j];
@@ -14,7 +12,6 @@ public class Solution {
                 dp[j] = Math.max(left, right);
             }
         }
-
         return dp[n - 1] >= 0;
     }
 }
